0ff87145ab94f70e8352c1b8bede036fe35a09cf
Mateusz Giełżecki <mateuszgielzecki@gmail.com>
2021-07-27 16:45:12 +0200

    new structure

--- script.js
+++
@@ -1,6 +0,0 @@
-const configurationPanel = document.querySelector(".configuration-panel");
-const configurationBtn = document.querySelector(".fa-bars");
-
-configurationBtn.addEventListener("click", () => {
-  configurationPanel.classList.toggle("active");
-});
