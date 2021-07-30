ecb84b01ac4b88a08fb29aea28fdf8d64644c7fa
Mateusz Giełżecki <mateuszgielzecki@gmail.com>
2021-07-30 18:38:28 +0200

    add new functionality - deleting element

--- script.js
+++ script.js
@@ -3,15 +3,46 @@ const submitBtn = document.querySelector('.submit-btn');
 const listOfProducts = document.querySelector('.list-of-products');
 const errorContainer = document.querySelector('.error-container');
 
+
+const item = [...document.querySelectorAll('.item')];
+const deletedItem = [...document.querySelectorAll('.fa-times')];
+
+
+
+// productCategory.forEach(cat => {
+//     if (cat.childNodes.length === 1) cat.remove();
+//     // console.log(cat.childNodes[1]);
+
+// })
+
+item.forEach(item => {
+    item.addEventListener('click', () => {
+        item.classList.toggle('mark')
+
+
+    })
+})
+
+deletedItem.forEach(item => {
+    item.addEventListener('click', () => {
+        // item.remove()
+        item.closest('li').remove();
+
+        const productCategory = [...document.querySelectorAll('.product-category')];
+
+        productCategory.forEach(cat => {
+            if (cat.childElementCount === 0) cat.remove();
+            console.dir(cat.childElementCount);
+
+        })
+
+    })
+})
+
+
+
+
 // Add new category
-
-
-const errorVisible = setTimeout(() => {
-    errorContainer.classList.remove('active')
-
-
-}, 7000);
-
 let newLiCategory
 const addNewCategory = function () {
     const newCategory = categoryField.value;
@@ -25,19 +56,23 @@ const addNewCategory = function () {
         </ul>`);
         categoryField.value = '';
     } else {
-        errorContainer.classList.add('active')
-        errorVisible();
-    }
+        errorContainer.classList.add('active');
 
-}
-
+        setTimeout(() => {
+            errorContainer.classList.remove('active');
+        }, 7000);
+    };
+};
 
 const clickBtn = function (e) {
     e.preventDefault();
 
     addNewCategory();
 
-}
+};
+
+
+
 
 categoryField.addEventListener('keydown', () => errorContainer.classList.remove('active'))
 submitBtn.addEventListener('click', clickBtn);
