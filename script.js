e1b7895f7a19f0141fef1797f4326ef3b4a43068
Mateusz Giełżecki <mateuszgielzecki@gmail.com>
2021-08-12 16:59:40 +0200

    .

--- script.js
+++ script.js
@@ -1,6 +1,83 @@
-const configurationPanel = document.querySelector(".configuration-panel");
-const configurationBtn = document.querySelector(".fa-bars");
+const categoryField = document.getElementById("category");
+const submitBtn = document.querySelector(".submit-btn");
+const listOfProducts = document.querySelector(".list-of-products");
+const errorContainer = document.querySelector(".error-container");
 
-configurationBtn.addEventListener("click", () => {
-  configurationPanel.classList.toggle("active");
+const inputs = [
+  ...document.querySelectorAll(".list-of-products li ul li input"),
+];
+const mainLi = [...document.querySelectorAll(".list-of-products li ul li")];
+
+let dataNumber = 1;
+mainLi.forEach((li) => li.setAttribute("data-number", `${dataNumber++}`));
+
+const mainList = document.querySelector(".main-list");
+
+inputs.forEach((input) => {
+  input.addEventListener("click", () => {
+    if (input.checked === true) {
+      const inputClosest = input.closest("li");
+      inputClosest.classList.add("active");
+      const itemValue = input.closest("li").textContent;
+
+      mainList.insertAdjacentHTML(
+        "beforeend",
+        `<li class='item' data-number="${inputClosest.attributes[0].value}"><i class="fas fa-times"></i>${itemValue}</li>`
+      );
+      markAndDelete();
+    } else {
+      input.closest("li").classList.remove("active");
+    }
+  });
 });
+
+const markAndDelete = function () {
+  let deletedItem = [...document.querySelectorAll(".fa-times")];
+  let item = [...document.querySelectorAll(".item")];
+
+  item.forEach((item) => {
+    item.addEventListener("click", () => {
+      item.classList.add("mark");
+    });
+  });
+
+  deletedItem.forEach((item) => {
+    item.addEventListener("click", () => {
+      item.closest("li").remove();
+    });
+  });
+};
+
+// Add new category
+let newLiCategory;
+const addNewCategory = function () {
+  const newCategory = categoryField.value;
+
+  if (newCategory) {
+    newLiCategory = document.createElement("li");
+    listOfProducts.appendChild(newLiCategory);
+    newLiCategory.insertAdjacentHTML(
+      "beforeend",
+      `<ul>
+        <i class="fas fa-puzzle-piece"></i> ${newCategory}:
+        </ul>`
+    );
+    categoryField.value = "";
+  } else {
+    errorContainer.classList.add("active");
+
+    setTimeout(() => {
+      errorContainer.classList.remove("active");
+    }, 7000);
+  }
+};
+
+const clickBtn = function (e) {
+  e.preventDefault();
+  addNewCategory();
+};
+
+categoryField.addEventListener("keydown", () =>
+  errorContainer.classList.remove("active")
+);
+submitBtn.addEventListener("click", clickBtn);
